ea6936513b99a1a42c77c9497e1f76cfe10612b4
Lalit Chauhan <73524110+LalitChauhan56@users.noreply.github.com>
2023-05-21 22:17:40 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,18 +11,18 @@ using TestPackage, Test
 
 end
 
-# define Reaction (refer to Catalyst for the full answer)
-struct Reaction
-    r
-    p
-end
+# # define Reaction (refer to Catalyst for the full answer)
+# struct Reaction
+#     r
+#     p
+# end
 
-# take an example reaction and start to balance it 
-# http://mathgene.usc.es/matlab-profs-quimica/reacciones.pdf
-# OH- + H+ -> H2O
-[1,1] [1]
-r = ["water" , "CO2"]
-r = Dict(["water"=>1 , "CO2"=>3]
+# # take an example reaction and start to balance it 
+# # http://mathgene.usc.es/matlab-profs-quimica/reacciones.pdf
+# # OH- + H+ -> H2O
+# [1,1] [1]
+# r = ["water" , "CO2"]
+# r = Dict(["water"=>1 , "CO2"=>3]
 
-balance(reaction) = TODO
+# balance(reaction) = TODO
 
