f799e9b42d742a4d58addf0f17f0bf151c439d4b
Lalit Chauhan <lalitchauhanlucky@gmail.com>
2023-07-06 23:27:05 +0530

    Creating a balanced Catalyst reaaction

--- src/testbalance.jl
+++ src/testbalance.jl
@@ -28,7 +28,7 @@ function parse_formula(formula)
 end
 
 function balance_reaction(reaction)
-    split_reaction = split(reaction, "→")
+    split_reaction = split(reaction, "-->")
     reactants = split(split_reaction[1], "+")
     products = split(split_reaction[2], "+")
 
@@ -73,9 +73,74 @@ function balance_reaction(reaction)
     balanced_reactants = [string(coeffs[j], reactants[j]) for j in 1:length(reactants)]
     balanced_products = [string(coeffs[j + length(reactants)], products[j]) for j in 1:length(products)]
     
-    balanced_reaction = join(balanced_reactants, " + ") * " → " * join(balanced_products, " + ")
+    balanced_reaction = join(balanced_reactants, "+") * "-->" * join(balanced_products, "+")
+    
+    # Remove all whitespaces
+    balanced_reaction = replace(balanced_reaction, " " => "")
     
     return balanced_reaction
 end
 
+
 export balance_reaction
+
+function extract_coefficients(reaction_str)
+    reactions = split(reaction_str, "-->") 
+
+    # get reactants and coefficients
+    reactants_info = split(reactions[1], "+")
+    reactant_coefficients = [parse(Int, r[1]) for r in reactants_info]
+
+    # get products and coefficients
+    products_info = split(reactions[2], "+")
+    product_coefficients = [parse(Int, p[1]) for p in products_info]
+
+    reactant_coefficients, product_coefficients
+end
+
+# reaction_str = "2H2+1O2-->2H2O" # your chemical equation here
+# reactant_coeff, product_coeff = extract_coefficients(reaction_str)
+# Gives 2 arrays with stoich values for reactants and products respectively
+# Assumes the coefficients to be always single digit for now
+
+function extract_species(reaction_str)
+    reactions = split(reaction_str, "-->") 
+
+    # get reactants and species
+    reactants_info = split(reactions[1], "+")
+    reactant_species = [String(r[2:end]) for r in reactants_info]
+
+    # get products and species
+    products_info = split(reactions[2], "+")
+    product_species = [String(p[2:end]) for p in products_info]
+
+    reactant_arr = vcat([eval(Meta.parse("@species $species(t)")) for species in reactant_species]...)
+    product_arr = vcat([eval(Meta.parse("@species $species(t)")) for species in product_species]...)
+
+    reactant_arr, product_arr
+end
+
+# reaction_str = "2H2+1O2-->2H2O" # your chemical equation here
+# reactant_coeff, product_coeff = extract_species(reaction_str)
+# Gives 2 arrays with species values for reactants and products respectively:(Num[H2(t), O2(t)], Num[H2O(t)]) 
+# Automatically creates the species required for the reaction 
+
+Reaction(k,reactant_arr  ,product_arr,reactant_coeff, product_coeff) #WORKS
+
+function create_balanced_reaction(reaction_str)
+    # Balance the reaction.
+    balanced_reaction_str = balance_reaction(reaction_str)
+
+    # Extract the coefficients and species from the balanced reaction.
+    reactant_coeff, product_coeff = extract_coefficients(balanced_reaction_str)
+    reactant_arr, product_arr = extract_species(balanced_reaction_str)
+
+    # Create and return the balanced reaction.
+    return Reaction(k, reactant_arr, product_arr, reactant_coeff, product_coeff)
+end
+
+# Example usage
+# reaction = create_balanced_reaction("CO2 + H2O --> C6H12O6 + O2") gives k, 6*CO2 + 6*H2O --> C6H12O6 + 6*O2
+# typeof(reaction) gives Reaction{Any, Int64}
+# println(reaction)
+# add this balanced reaction to a reaction network using Catalyst.!addreaction
--- test/runtests.jl
+++ test/runtests.jl
@@ -26,17 +26,17 @@ end
 
 @testset "balance_reaction" begin
 # Test balance_reaction
-reaction1 = balance_reaction("H2 + O2 → H2O")
-@test reaction1 == "2H2 + 1O2 → 2H2O"
+reaction1 = balance_reaction("H2 + O2 --> H2O")
+@test reaction1 == "2H2+1O2-->2H2O"
 
-reaction2 = balance_reaction("C + O2 → CO2")
-@test reaction2 == "1C + 1O2 → 1CO2"
+reaction2 = balance_reaction("C + O2 --> CO2")
+@test reaction2 == "1C+1O2-->1CO2"
 
-reaction3 = balance_reaction("CO2 + H2O → C6H12O6 + O2")
-@test reaction3 == "6CO2 + 6H2O → 1C6H12O6 + 6O2"
+reaction3 = balance_reaction("CO2 + H2O --> C6H12O6 + O2")
+@test reaction3 == "6CO2+6H2O-->1C6H12O6+6O2"
 
-reaction4 = balance_reaction("H2 + Cl2 → HCl")
-@test reaction4 == "1H2 + 1Cl2 → 2HCl"
+reaction4 = balance_reaction("H2 + Cl2 --> HCl")
+@test reaction4 == "1H2+1Cl2-->2HCl"
 
 end
 
