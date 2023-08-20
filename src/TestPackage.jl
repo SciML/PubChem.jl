3714753ddbe72aac9703cf5efde81d5703bdc037
Lalit Chauhan <lalitchauhanlucky@gmail.com>
2023-08-20 21:30:40 +0530

    Add Attach_metadata macro and improve extract_properties function
    Cleanup moved code, improve extract_properties function to work using names and not indices(more accurate) , add Attach_Metadata macro and properties function to get attached pubchem information

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -1,18 +1,12 @@
 module TestPackage
 using HTTP,JSON
 using JSON3
-using LinearAlgebra
-
-include("metadata.jl")
-include("testbalance.jl")
+using Catalyst
 
 
-export get_compound_properties
-export Compound
-export balance_reaction
-export parse_formula
-export get_json_from_cid
-export get_json_from_name
-export get_json_from_url
+include("Fetch_data.jl")
+
+export extract_properties
+export get_compound
 
 end
