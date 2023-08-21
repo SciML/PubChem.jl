05325f1841c3e70dc1e4763aa23dad27dfee9a12
Lalit Chauhan <lalitchauhanlucky@gmail.com>
2023-08-21 21:02:08 +0530

    Change package name

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-using TestPackage, Test
+using PubChem, Test
 
-@testset "TestPackage.jl" begin
+@testset "PubChem" begin
     a = get_compound(962)
     b = get_compound("water")
     
@@ -11,22 +11,4 @@ using TestPackage, Test
 
 end
 
-
-
-# # define Reaction (refer to Catalyst for the full answer)
-# struct Reaction
-#     r
-#     p
-# end
-
-# dict,array
-
-# # take an example reaction and start to balance it 
-# # http://mathgene.usc.es/matlab-profs-quimica/reacciones.pdf
-# # OH- + H+ -> H2O
-# [1,1] [1]
-# r = ["water" , "CO2"]
-# r = Dict(["water"=>1 , "CO2"=>3]
-
-# balance(reaction) = TODO
 
