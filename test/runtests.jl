7e1cf7d6a9292635bc0e2bfbfa714f9da846fcb5
Lalit Chauhan <lalitchauhanlucky@gmail.com>
2023-08-21 23:35:35 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using  Catalyst, HTTP, JSON, Test, SafeTestsets ,PubChem
 
 ### Run the tests ###
 @time begin
-    @time @safetestset "JSON" begin include("Interface.jl") end
+    @time @safetestset "JSON" begin include("interface.jl") end
     @time @safetestset "Chemistry" begin include("functions.jl") end
 end 
 
